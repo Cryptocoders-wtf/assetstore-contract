715989cde6e2813ff2b0d61911044c18e2259380
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-07 08:02:29 -0700

    markRead/Deleted

--- contracts/MessageBox.sol
+++ contracts/MessageBox.sol
@@ -2,14 +2,17 @@
 
 pragma solidity ^0.8.6;
 
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 import { IMessageBox, Message } from './interfaces/IMessageBox.sol';
 
-contract MessageBox is IMessageBox {
+contract MessageBox is Ownable, IMessageBox {
   mapping(address => Message[]) messages;
   mapping(address => uint256) counts;
 
 	function send(address _to, Message memory _message) external override returns (uint256) {
     require(msg.sender == _message.sender);
+    _message.isRead = false;
+    _message.isDeleted = false;
     Message[] storage queue = messages[_to];
     uint256 index = counts[_to];
     queue[index] = _message;
@@ -23,5 +26,17 @@ contract MessageBox is IMessageBox {
 
 	function get(address _to, uint256 _index) external view override returns (Message memory) {
     return messages[_to][_index];
+  }
+
+	function markRead(address _to, uint256 _index, bool _isRead) external override returns (Message memory) {
+    Message storage message = messages[_to][_index];
+    message.isRead = _isRead;
+    return message;
+  }
+
+	function markDeleted(address _to, uint256 _index, bool _isDeleted) external override returns (Message memory) {
+    Message storage message = messages[_to][_index];
+    message.isDeleted = _isDeleted;
+    return message;
   }
 }
--- contracts/interfaces/IMessageBox.sol
+++ contracts/interfaces/IMessageBox.sol
@@ -8,12 +8,16 @@ struct Message {
 	address sender;    // sender
 	string message;    // text representation
 	string url;        // icon
+	bool isRead;
+	bool isDeleted;
 }
 
 interface IMessageBox {
 	function send(address _to, Message memory _message) external returns (uint256);
 	function count(address _to) external returns (uint256);
 	function get(address _to, uint256 _index) external returns (Message memory);
+	function markRead(address _to, uint256 _index, bool _isRead) external returns (Message memory);
+	function markDeleted(address _to, uint256 _index, bool _isDeleted) external returns (Message memory);
 }
 
 interface ISpamFilter {
