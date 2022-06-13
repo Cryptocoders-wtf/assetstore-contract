8a75b4e55c0bfef23d9673313c569b3d7cb87444
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-13 08:11:13 -0700

    better test

--- test/index.ts
+++ test/index.ts
@@ -1,18 +1,15 @@
-//import { expect } from "chai";
+import { expect } from "chai";
 import { ethers } from "hardhat";
 
-async function main() {
+describe("Token contract", function () {
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
     const Greeter = await ethers.getContractFactory("contracts/libs/Base64.sol:Base64");
     const greeter = await Greeter.deploy();
     await greeter.deployed();
-
+  
     const result = await greeter.encode("Hello World");
+    expect(atob(result)).to.equal("Hello World");
     console.log(result);
-
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
-}
-
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+  });
 });
+
