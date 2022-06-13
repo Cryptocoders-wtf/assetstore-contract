91713658481958a7291f78c6a0faf2b1dac03ed8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-13 08:26:35 -0700

    test loop

--- test/index.ts
+++ test/index.ts
@@ -1,15 +1,20 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const Greeter = await ethers.getContractFactory("contracts/libs/Base64.sol:Base64");
-    const greeter = await Greeter.deploy();
-    await greeter.deployed();
+describe("Base64", function () {
+  it("Verify encode", async function () {
+    const Base64Factory = await ethers.getContractFactory("contracts/libs/Base64.sol:Base64");
+    const base64 = await Base64Factory.deploy();
+    await base64.deployed();
   
-    const result = await greeter.encode("Hello World");
-    expect(atob(result)).to.equal("Hello World");
-    console.log(result);
+    const messages = [
+      "Hello World",
+      "<svg>foobar</svg>",
+    ];
+    const processes = messages.map(async (message) => {
+      const result = await base64.encode(message);
+      expect(atob(result)).to.equal(message);
+    });
+    return Promise.all(processes);
   });
 });
-
