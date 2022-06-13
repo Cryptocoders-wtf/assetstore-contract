cc5c3a445b1de51a8f6c12b46ca4d2efffa5e398
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-13 10:26:24 -0700

    VectorToken

---
+++ contracts/VectorToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/*********************************
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░██░░░████░░██░░░████░░░ *
+ * ░░██████░░░████████░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ *********************************/
+
+pragma solidity ^0.8.6;
+
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import { ERC721 } from './base/ERC721.sol';
+import { ERC721Enumerable } from './base/ERC721Enumerable.sol';
+
+contract VectorToken is Ownable, ERC721Enumerable {
+    constructor() ERC721('VectorToken', 'VECTORTOKEN') {
+    }
+}
+
