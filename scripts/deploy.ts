a2854e904a7dbdf02b24118b7eb880ed20d281e3
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-13 22:52:24 -0700

    clean up

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,14 +5,6 @@
 // Runtime Environment's members available in the global scope.
 import { ethers, network } from "hardhat";
 
-const waitForUserInput = (text: string) => {
-  return new Promise((resolve, reject) => {
-    process.stdin.resume()
-    process.stdout.write(text)
-    process.stdin.once('data', data => resolve(data.toString().trim()))
-  })
-};
-
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
@@ -23,40 +15,14 @@ async function main() {
 
   const isRinkeby = network.name == "rinkeby";
   console.log("deploying...", network.name, isRinkeby ? "testnet": "*", process.env.ALCHEMY_API_KEY);
-
-  const minter = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
-  const descriptor = isRinkeby ? "0x292c84894c1B86140A784eec99711d6007005f21":"0x0cfdb3ba1694c2bb2cfacb0339ad7b1ae5932b63";
-  const seeder = isRinkeby ? "0x5bcc91c44bffa15c9b804a5fd30174e8da296a4b":"0xcc8a0fb5ab3c7132c1b2a0109142fb112c4ce515";
-  const proxy = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
-  const developer = "0x818Fb9d440968dB9fCB06EEF53C7734Ad70f6F0e"; // ai
-  const committee = "0x4E4cD175f812f1Ba784a69C1f8AC8dAa52AD7e2B";
-
-  console.log("descriptor: ", descriptor);
-  console.log("seeder:     ", seeder)
-  const input = await waitForUserInput("Are you ok?(Y/n)");
-  if (input != "Y") {
-    process.exit()
-    return 
-  }
-  // await deployer.deploy(NFT, minter, descriptor, seeder, developers, proxy);
-
-  // 1 eth = 10**18
-  const priceSeed = {
-    maxPrice:  String(10 ** 16), // 0.01 ether; = 1 * 10^2
-    minPrice:  String(5 * 10 ** 13), //  0.00005 ether; = 5 * 10^-5
-    priceDelta:  String(15 * 10 ** 13), // 0.00015 ether; = 15 * 10^-5
-    timeDelta: 60, // 1 minutes; 
-    expirationTime: 90 * 60, // 90 minutes;
-  };
   
   // We get the contract to deploy
-  const NounsToken = await ethers.getContractFactory("NounsvilleToken");
-  const nounsToken = await NounsToken.deploy(descriptor, seeder, developer, committee, priceSeed, proxy);
+  const factory = await ethers.getContractFactory("PrideSquiggle");
+  const contract = await factory.deploy();
 
-  await nounsToken.deployed();
-
+  await contract.deployed();
   
-  console.log("nounsToken deployed to:", nounsToken.address);
+  console.log("VectorToken deployed to:", contract.address);
   process.exit()
 }
 
--- scripts/deploy_old.ts
+++ scripts/deploy_old.ts
@@ -24,19 +24,39 @@ async function main() {
   const isRinkeby = network.name == "rinkeby";
   console.log("deploying...", network.name, isRinkeby ? "testnet": "*", process.env.ALCHEMY_API_KEY);
 
+  const minter = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+  const descriptor = isRinkeby ? "0x292c84894c1B86140A784eec99711d6007005f21":"0x0cfdb3ba1694c2bb2cfacb0339ad7b1ae5932b63";
+  const seeder = isRinkeby ? "0x5bcc91c44bffa15c9b804a5fd30174e8da296a4b":"0xcc8a0fb5ab3c7132c1b2a0109142fb112c4ce515";
+  const proxy = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
+  const developer = "0x818Fb9d440968dB9fCB06EEF53C7734Ad70f6F0e"; // ai
+  const committee = "0x4E4cD175f812f1Ba784a69C1f8AC8dAa52AD7e2B";
+
+  console.log("descriptor: ", descriptor);
+  console.log("seeder:     ", seeder)
   const input = await waitForUserInput("Are you ok?(Y/n)");
   if (input != "Y") {
     process.exit()
     return 
   }
+  // await deployer.deploy(NFT, minter, descriptor, seeder, developers, proxy);
+
+  // 1 eth = 10**18
+  const priceSeed = {
+    maxPrice:  String(10 ** 16), // 0.01 ether; = 1 * 10^2
+    minPrice:  String(5 * 10 ** 13), //  0.00005 ether; = 5 * 10^-5
+    priceDelta:  String(15 * 10 ** 13), // 0.00015 ether; = 15 * 10^-5
+    timeDelta: 60, // 1 minutes; 
+    expirationTime: 90 * 60, // 90 minutes;
+  };
   
   // We get the contract to deploy
-  const factory = await ethers.getContractFactory("MessageBox");
-  const contract = await factory.deploy();
+  const NounsToken = await ethers.getContractFactory("NounsvilleToken");
+  const nounsToken = await NounsToken.deploy(descriptor, seeder, developer, committee, priceSeed, proxy);
 
-  await contract.deployed();
+  await nounsToken.deployed();
+
   
-  console.log("MessageBox deployed to:", contract.address);
+  console.log("nounsToken deployed to:", nounsToken.address);
   process.exit()
 }
 
