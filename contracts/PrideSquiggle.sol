830fd6470a27e3ee9be49a095bdf00f92b166ec0
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-14 13:18:50 -0700

    constructor with developer and proxy

--- contracts/PrideSquiggle.sol
+++ contracts/PrideSquiggle.sol
@@ -16,19 +16,48 @@
 pragma solidity ^0.8.6;
 
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
-import { ERC721 } from './base/ERC721.sol';
 import { ERC721Enumerable } from './base/ERC721Enumerable.sol';
-import { Base64 } from 'base64-sol/base64.sol';
 import { INounsToken } from './interfaces/INounsToken.sol';
+import { ERC721 } from './base/ERC721.sol';
+import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import { Base64 } from 'base64-sol/base64.sol';
+import { IProxyRegistry } from './external/opensea/IProxyRegistry.sol';
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract PrideSquiggle is INounsToken, Ownable, ERC721Enumerable {
   using Strings for uint256;
+
+  // The internal noun ID tracker
   uint256 private _currentNounId;
 
-  constructor() ERC721('VectorToken', 'VECTORTOKEN') {
+  // developer address.
+  address public developer;
+
+  // OpenSea's Proxy Registry
+  IProxyRegistry public immutable proxyRegistry;
+
+  constructor(
+      address _developer,
+      IProxyRegistry _proxyRegistry
+    ) ERC721('VectorToken', 'VECTORTOKEN') {
+      developer = _developer;
+      proxyRegistry = _proxyRegistry;
   }
 
+  /**
+    * @notice Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-less listings.
+    */
+  function isApprovedForAll(address owner, address operator) public view override(IERC721, ERC721) returns (bool) {
+      // Whitelist OpenSea proxy contract for easy trading.
+      if (proxyRegistry.proxies(owner) == operator) {
+          return true;
+      }
+      return super.isApprovedForAll(owner, operator);
+  }
+
+  /**
+    * @notice Anybody can mint, one per wallet.
+    */
   function mint() public override returns (uint256) {
     require(balanceOf(msg.sender) == 0, "You already have one.");
     uint256 tokenId = _currentNounId++;
@@ -37,10 +66,20 @@ contract PrideSquiggle is INounsToken, Ownable, ERC721Enumerable {
     return tokenId;
   }
 
+  /**
+    * @notice Burn a noun.
+    */
   function burn(uint256 nounId) public override onlyOwner {
     require(_exists(nounId), 'URI query for nonexistent token');
     _burn(nounId);
     emit NounBurned(nounId);
+  }
+
+  /*
+    * @notice get next tokenId.
+    */
+  function getCurrentToken() external view returns (uint256) {                  
+      return _currentNounId;
   }
 
   function generateSVG(uint256 tokenId) public pure returns (string memory) {
@@ -110,8 +149,8 @@ contract PrideSquiggle is INounsToken, Ownable, ERC721Enumerable {
   }
 
   /**
-    * @notice A distinct Uniform Resource Identifier (URI) for a given asset.
-    * @dev See {IERC721Metadata-tokenURI}.
+    * @notice Similar to `tokenURI`, but always serves a base64 encoded data URI
+    * with the JSON contents directly inlined.
     */
   function dataURI(uint256 tokenId) public view override returns (string memory) {
     require(_exists(tokenId), 'NounsToken: URI query for nonexistent token');
@@ -131,8 +170,20 @@ contract PrideSquiggle is INounsToken, Ownable, ERC721Enumerable {
     );
   }
 
+  /**
+    * @notice A distinct Uniform Resource Identifier (URI) for a given asset.
+    * @dev See {IERC721Metadata-tokenURI}.
+    */
   function tokenURI(uint256 tokenId) public view override returns (string memory) {
     return dataURI(tokenId);
+  }
+
+  /**
+    * @notice Set developer.
+    * @dev Only callable by the Owner.
+    */
+  function setDeveloper(address _developer) external onlyOwner {
+      developer = _developer;
   }
 }
 
