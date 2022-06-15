8db3753c31b404aec996bb8a3e07d7f6fdf36120
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-14 21:13:59 -0700

    test 0-10000

---
+++ scripts/svgall.ts
@@ -0,0 +1,21 @@
+
+import { ethers, network } from "hardhat";
+
+async function main() {
+  const factory = await ethers.getContractFactory("PrideSquiggle");
+  const limit = 10000;
+  const developer = "0x6a615Ca8D7053c0A0De2d11CACB6f321CA63BD62"; // sn2
+  const proxy = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
+  const contract = await factory.deploy(limit, developer, proxy);
+  await contract.deployed();
+  let i;
+  for (i=0; i<10000; i++) {
+    const result = await contract.generateSVG(i);
+    console.log(i);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
