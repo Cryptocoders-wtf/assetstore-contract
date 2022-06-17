f7e21e1345b425852cbb859ece9cc3bfd98f16d0
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-16 21:31:51 -0700

    url.ts

---
+++ scripts/url.ts
@@ -0,0 +1,20 @@
+
+import { ethers, network } from "hardhat";
+
+async function main() {
+  const factory = await ethers.getContractFactory("PrideSquiggle");
+  const limit = 10000;
+  const developer = "0x6a615Ca8D7053c0A0De2d11CACB6f321CA63BD62"; // sn2
+  const proxy = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
+  const contract = await factory.deploy(limit, developer, proxy);
+  await contract.deployed();
+  await contract.mint();
+  const result = await contract.tokenURI(0);
+  console.log(result);
+  //console.log('data:image/svg+xml;base64,' + btoa(result));
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
