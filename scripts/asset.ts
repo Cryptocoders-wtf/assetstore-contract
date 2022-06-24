6ae829481c1e76f9636be582b0410ac9b7629af1
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-24 10:27:46 +0900

    deal with variable height and width

--- scripts/asset.ts
+++ scripts/asset.ts
@@ -1,19 +1,24 @@
 
 import { SSL_OP_EPHEMERAL_RSA } from "constants";
 import { ethers, network } from "hardhat";
-import { actionAssets } from "../data/materials";
+import { actionAssets, socialAssets } from "../data/materials";
 import { deploy } from "../utils/deploy";
 
 async function main() {
   const { assetStore, materialToken } = await deploy();
 
-  const promises:Array<Promise<any>> = actionAssets.map(asset => {
+  let promises:Array<Promise<any>> = actionAssets.map(asset => {
+    return materialToken.mint(asset, 0);
+  });
+  await Promise.all(promises);
+
+  promises = socialAssets.map(asset => {
     return materialToken.mint(asset, 0);
   });
   await Promise.all(promises);
 
   if (network.name == "hardhat" || network.name == "localhost") {
-    const uri = await materialToken.tokenURI(actionAssets.length * 2 - 1);
+    const uri = await materialToken.tokenURI(actionAssets.length * 2 + socialAssets.length * 2 - 2);
     const data = atob(uri.substring(29));
     const json = JSON.parse(data);
     const imageData = json.image.substring(26);
