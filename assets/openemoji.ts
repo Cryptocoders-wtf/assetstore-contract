e9d915454fc945eb0e0d660bc144db96fc224b38
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-25 13:48:42 +0900

    multi color

--- assets/openemoji.ts
+++ assets/openemoji.ts
@@ -4,6 +4,9 @@ const emojis = [{
   name: "Done",
   parts: [{
     mask: "", color: "red",
+    body: "M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z",
+  },{
+    mask: "", color: "blue",
     body: "M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"
   }]
 }];
