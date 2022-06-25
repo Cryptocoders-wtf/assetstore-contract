bb05e8b162f70b2ecd0ac34455a7e31561db6d29
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-25 15:52:10 +0900

    gas fees for action and social icons

--- scripts/gas.ts
+++ scripts/gas.ts
@@ -7,11 +7,25 @@ async function main() {
   const { assetStore, materialToken } = await deploy();
   const [owner] = await ethers.getSigners();
 
-  const asset = actionAssets[0];
-  asset.soulbound = owner.address;
-  const tx = await materialToken.mint(asset, 0);
-  const result = await tx.wait();
-  console.log(result.gasUsed.toNumber());
+  console.log("actionAssets");
+  let promises = actionAssets.map(async (asset) => {
+    asset.soulbound = owner.address;
+    const tx = await materialToken.mint(asset, 0);
+    const result = await tx.wait();
+    return result.gasUsed.toNumber();
+  });
+  let returns = await Promise.all(promises);
+  console.log(returns);
+
+  console.log("socialAssets");
+  promises = socialAssets.map(async (asset) => {
+    asset.soulbound = owner.address;
+    const tx = await materialToken.mint(asset, 0);
+    const result = await tx.wait();
+    return result.gasUsed.toNumber();
+  });
+  returns = await Promise.all(promises);
+  console.log(returns);
 }
 
 main().catch((error) => {
