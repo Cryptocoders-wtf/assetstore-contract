de682d6512f94a1ff0bacd82b990347b66c4c955
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-25 19:42:17 +0900

    new gas data

---
+++ gas/002_4d1cf0a.ts
@@ -0,0 +1,20 @@
+No need to generate any newer typings.
+const gas = {
+  action: [
+    { Unit: 791249, ETH: 0.03956, USD: 39.56 },
+    { Unit: 1694753, ETH: 0.08474, USD: 84.74 },
+    { Unit: 906528, ETH: 0.04533, USD: 45.33 },
+    { Unit: 637941, ETH: 0.0319, USD: 31.9 },
+    { Unit: 887575, ETH: 0.04438, USD: 44.38 },
+    { Unit: 809311, ETH: 0.04047, USD: 40.47 }
+  ],
+  social: [
+    { Unit: 1876407, ETH: 0.09382, USD: 93.82 },
+    { Unit: 2459483, ETH: 0.12297, USD: 122.97 },
+    { Unit: 2825926, ETH: 0.1413, USD: 141.3 },
+    { Unit: 1249056, ETH: 0.06245, USD: 62.45 },
+    { Unit: 1878877, ETH: 0.09394, USD: 93.94 },
+    { Unit: 1570893, ETH: 0.07854, USD: 78.54 }
+  ],
+  emoji: [ { Unit: 11171286, ETH: 0.55856, USD: 558.56 } ]
+} ;
