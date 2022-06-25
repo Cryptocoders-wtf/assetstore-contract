1ac74122cd9af85ffcb9896f199d89acffd97593
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-25 17:40:46 +0900

    50wei/unit

--- utils/math.ts
+++ utils/math.ts
@@ -1,8 +1,9 @@
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
 export const gasEstimate = (t3:any) => { 
     const gasUsed = t3.gasUsed;
-    const gasPrice = t3.effectiveGasPrice;
+    const gasPrice = BigNumber.from(50 * 1.0E9); // 50gwei
     const gasCost = gasUsed.mul(gasPrice);
     const gasCostEth = Number.parseFloat(ethers.utils.formatEther(gasCost));
     const ETHUSD = 1000; // assume eth is $1000 usd
