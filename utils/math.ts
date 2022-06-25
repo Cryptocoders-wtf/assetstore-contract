4e471a5f48c91f21aa58ea7ab5e133c1bb8b2157
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-25 19:21:41 +0900

    rounded numbers

--- utils/math.ts
+++ utils/math.ts
@@ -5,8 +5,8 @@ export const gasEstimate = (t3:any) => {
     const gasUsed = t3.gasUsed;
     const gasPrice = BigNumber.from(50 * 1.0E9); // 50gwei
     const gasCost = gasUsed.mul(gasPrice);
-    const gasCostEth = Number.parseFloat(ethers.utils.formatEther(gasCost));
-    const ETHUSD = 1000; // assume eth is $1000 usd
-    const gasCostUsd = gasCostEth * ETHUSD;
+    const gasCostEth = Math.round(Number.parseFloat(ethers.utils.formatEther(gasCost)) * 100000)/100000;
+    const ETHUSD = 1200; // assume eth is $1200 usd
+    const gasCostUsd = Math.round(gasCostEth * ETHUSD * 100) / 100;
     return { Unit: gasUsed.toNumber(), ETH: gasCostEth, USD: gasCostUsd };
 };
