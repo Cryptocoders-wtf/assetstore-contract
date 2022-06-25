eb67abd15628a7adb079f6986ddedc8c05d5a726
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-25 19:32:28 +0900

    gas starting point

---
+++ gas/11_2e7a26c.ts
@@ -0,0 +1,20 @@
+No need to generate any newer typings.
+const gas = {
+  action: [
+    { Unit: 791264, ETH: 0.03956, USD: 39.56 },
+    { Unit: 1717513, ETH: 0.08588, USD: 85.88 },
+    { Unit: 929288, ETH: 0.04646, USD: 46.46 },
+    { Unit: 660701, ETH: 0.03304, USD: 33.04 },
+    { Unit: 910335, ETH: 0.04552, USD: 45.52 },
+    { Unit: 832071, ETH: 0.0416, USD: 41.6 }
+  ],
+  social: [
+    { Unit: 1893527, ETH: 0.09468, USD: 94.68 },
+    { Unit: 2480531, ETH: 0.12403, USD: 124.03 },
+    { Unit: 2846974, ETH: 0.14235, USD: 142.35 },
+    { Unit: 1270104, ETH: 0.06351, USD: 63.51 },
+    { Unit: 1899925, ETH: 0.095, USD: 95 },
+    { Unit: 1591941, ETH: 0.0796, USD: 79.6 }
+  ],
+  emoji: [ { Unit: 11171301, ETH: 0.55857, USD: 558.57 } ]
+} ;
