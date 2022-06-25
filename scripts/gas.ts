2e7a26c6b5d6d53d7f50340fd8225a3bd2c289e3
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-25 19:30:21 +0900

    output as json

--- scripts/gas.ts
+++ scripts/gas.ts
@@ -10,32 +10,27 @@ async function main() {
   const [owner] = await ethers.getSigners();
   const unitPrice = ethers.BigNumber.from(55);
 
-  console.log("actionAssets");
   let promises = actionAssets.map(async (asset) => {
     asset.soulbound = owner.address;
     const tx = await materialToken.mint(asset, 0);
     return tx.wait();
   });
-  let returns = await Promise.all(promises);
-  console.log(returns.map(gasEstimate));
+  const action = (await Promise.all(promises)).map(gasEstimate);
 
-  console.log("socialAssets");
   promises = socialAssets.map(async (asset) => {
     asset.soulbound = owner.address;
     const tx = await materialToken.mint(asset, 0);
     return tx.wait();
   });
-  returns = await Promise.all(promises);
-  console.log(returns.map(gasEstimate));
+  const social = (await Promise.all(promises)).map(gasEstimate);
 
-  console.log("openEmoji");
   promises = enojiAssets.map(async (asset) => {
     asset.soulbound = owner.address;
     const tx = await materialToken.mint(asset, 0);
     return tx.wait();
   });
-  returns = await Promise.all(promises);
-  console.log(returns.map(gasEstimate));
+  const emoji = (await Promise.all(promises)).map(gasEstimate);
+  console.log("const gas =", { action, social, emoji }, ";");
 }
 
 main().catch((error) => {
--- utils/math.ts
+++ utils/math.ts
@@ -6,7 +6,7 @@ export const gasEstimate = (t3:any) => {
     const gasPrice = BigNumber.from(50 * 1.0E9); // 50gwei
     const gasCost = gasUsed.mul(gasPrice);
     const gasCostEth = Math.round(Number.parseFloat(ethers.utils.formatEther(gasCost)) * 100000)/100000;
-    const ETHUSD = 1200; // assume eth is $1200 usd
+    const ETHUSD = 1000; // assume eth is $1000 usd
     const gasCostUsd = Math.round(gasCostEth * ETHUSD * 100) / 100;
     return { Unit: gasUsed.toNumber(), ETH: gasCostEth, USD: gasCostUsd };
 };
