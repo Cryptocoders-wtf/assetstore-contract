aef38a584e3ce14cbdb72ba9a9491e63b205d4df
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-26 10:42:21 +0900

    compressPath works again

--- assets/createAsset.ts
+++ assets/createAsset.ts
@@ -6,6 +6,12 @@ const assetBase:any = {
 const regex = /[+-\d\.]+/g;
 const encoder = new TextEncoder();
 
+const compressPath = (body:string, width:number) => {
+  return encoder.encode(body.replace(regex, (str:string)=>{
+    return `${Math.round(parseFloat(str) * 1000 / width)}`;
+  }));
+} 
+
 export const createAsset = (_asset:any, group:string, category:string) => {
   let asset = Object.assign({}, assetBase);
   asset.group = group;
@@ -16,19 +22,15 @@ export const createAsset = (_asset:any, group:string, category:string) => {
   asset.height = 1000;
   if (_asset.parts) {
     asset.parts = _asset.parts.map((part:any) => {
-      part.mask = encoder.encode(part.mask || "");
+      part.mask = compressPath(part.mask || "", width);
       part.color = part.color || "";
-      part.body = encoder.encode(part.body.replace(regex, (str:string)=>{
-        return Math.round(parseFloat(str) * 1000 / width);
-      }));
+      part.body = compressPath(part.body, width);
       return part;
     });
   } else {
     asset.parts = [{
-      mask: encoder.encode(""), color: "",
-      body: encoder.encode(_asset.body.replace(regex, (str:string)=>{
-        return Math.round(parseFloat(str) * 1000 / width);
-      }))
+      mask: compressPath("", width), color: "",
+      body: compressPath(_asset.body, width)
     }];
   }
   return asset;  
