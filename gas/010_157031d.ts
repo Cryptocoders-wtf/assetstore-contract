887770deec7c0dddd2af422e0b7f549fc8d57a30
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-27 08:00:41 +0900

    gas measured

---
+++ gas/010_157031d.ts
@@ -0,0 +1,23 @@
+
+> nounslove@1.0.0 gas
+> hardhat run ./scripts/gas.ts | sed '1d'
+
+const gas = {
+  action: [
+    { Unit: 747273, ETH: 0.03736, USD: 37.36 },
+    { Unit: 832984, ETH: 0.04165, USD: 41.65 },
+    { Unit: 677315, ETH: 0.03387, USD: 33.87 },
+    { Unit: 625602, ETH: 0.03128, USD: 31.28 },
+    { Unit: 672489, ETH: 0.03362, USD: 33.62 },
+    { Unit: 650890, ETH: 0.03254, USD: 32.54 }
+  ],
+  social: [
+    { Unit: 947773, ETH: 0.04739, USD: 47.39 },
+    { Unit: 990589, ETH: 0.04953, USD: 49.53 },
+    { Unit: 1058692, ETH: 0.05293, USD: 52.93 },
+    { Unit: 764468, ETH: 0.03822, USD: 38.22 },
+    { Unit: 901898, ETH: 0.04509, USD: 45.09 },
+    { Unit: 809042, ETH: 0.04045, USD: 40.45 }
+  ],
+  emoji: [ { Unit: 4345476, ETH: 0.21727, USD: 217.27 } ]
+} ;
