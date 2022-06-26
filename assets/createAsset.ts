ee4ac43bef504204aba63b6fcf2ada4664bf2bdb
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-26 10:38:13 +0900

    undo the last change

--- assets/createAsset.ts
+++ assets/createAsset.ts
@@ -6,13 +6,6 @@ const assetBase:any = {
 const regex = /[+-\d\.]+/g;
 const encoder = new TextEncoder();
 
-const convert:any = (body:string, width:number) => {
-  let ret:string = body.replace(regex, (str:string) => {
-    return String(Math.round(parseFloat(str) * 1000 / width));
-  });
-  return encoder.encode(ret);
-};
-
 export const createAsset = (_asset:any, group:string, category:string) => {
   let asset = Object.assign({}, assetBase);
   asset.group = group;
@@ -23,15 +16,19 @@ export const createAsset = (_asset:any, group:string, category:string) => {
   asset.height = 1000;
   if (_asset.parts) {
     asset.parts = _asset.parts.map((part:any) => {
-      part.mask = convert(part.mask || "");
+      part.mask = encoder.encode(part.mask || "");
       part.color = part.color || "";
-      part.body = convert(part.body);
+      part.body = encoder.encode(part.body.replace(regex, (str:string)=>{
+        return Math.round(parseFloat(str) * 1000 / width);
+      }));
       return part;
     });
   } else {
     asset.parts = [{
-      mask: convert(""), color: "",
-      body: convert(_asset.body)
+      mask: encoder.encode(""), color: "",
+      body: encoder.encode(_asset.body.replace(regex, (str:string)=>{
+        return Math.round(parseFloat(str) * 1000 / width);
+      }))
     }];
   }
   return asset;  
