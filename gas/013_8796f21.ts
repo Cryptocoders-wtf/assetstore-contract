4803973f99913b3715da7a5dcf88e9b20033ef03
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-27 17:23:46 +0900

    gas: getNameAtIndex

---
+++ gas/013_8796f21.ts
@@ -0,0 +1,23 @@
+
+> nounslove@1.0.0 gas
+> hardhat run ./scripts/gas.ts | sed '1d'
+
+const gas = {
+  action: [
+    { Unit: 890755, ETH: 0.04454, USD: 44.54 },
+    { Unit: 833491, ETH: 0.04167, USD: 41.67 },
+    { Unit: 677822, ETH: 0.03389, USD: 33.89 },
+    { Unit: 626109, ETH: 0.03131, USD: 31.31 },
+    { Unit: 672996, ETH: 0.03365, USD: 33.65 },
+    { Unit: 651397, ETH: 0.03257, USD: 32.57 }
+  ],
+  social: [
+    { Unit: 948174, ETH: 0.04741, USD: 47.41 },
+    { Unit: 991096, ETH: 0.04955, USD: 49.55 },
+    { Unit: 1059199, ETH: 0.05296, USD: 52.96 },
+    { Unit: 764975, ETH: 0.03825, USD: 38.25 },
+    { Unit: 902405, ETH: 0.04512, USD: 45.12 },
+    { Unit: 809549, ETH: 0.04048, USD: 40.48 }
+  ],
+  emoji: [ { Unit: 4345700, ETH: 0.21729, USD: 217.29 } ]
+} ;
