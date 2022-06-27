af05536b9294657a2dcec03138735b53faf9a62c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-27 17:13:44 +0900

    gas: effect of StringSet library

---
+++ gas/012_cca563a.ts
@@ -0,0 +1,23 @@
+
+> nounslove@1.0.0 gas
+> hardhat run ./scripts/gas.ts | sed '1d'
+
+const gas = {
+  action: [
+    { Unit: 890767, ETH: 0.04454, USD: 44.54 },
+    { Unit: 833503, ETH: 0.04168, USD: 41.68 },
+    { Unit: 677834, ETH: 0.03389, USD: 33.89 },
+    { Unit: 626121, ETH: 0.03131, USD: 31.31 },
+    { Unit: 673008, ETH: 0.03365, USD: 33.65 },
+    { Unit: 651409, ETH: 0.03257, USD: 32.57 }
+  ],
+  social: [
+    { Unit: 948186, ETH: 0.04741, USD: 47.41 },
+    { Unit: 991108, ETH: 0.04956, USD: 49.56 },
+    { Unit: 1059211, ETH: 0.05296, USD: 52.96 },
+    { Unit: 764987, ETH: 0.03825, USD: 38.25 },
+    { Unit: 902417, ETH: 0.04512, USD: 45.12 },
+    { Unit: 809561, ETH: 0.04048, USD: 40.48 }
+  ],
+  emoji: [ { Unit: 4345800, ETH: 0.21729, USD: 217.29 } ]
+} ;
