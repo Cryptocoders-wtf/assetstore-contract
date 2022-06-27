f898027eccca708d04f38285ec6e7b7e9f5ae9de
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-27 17:41:15 +0900

    four rectangles in multi parts test case

--- assets/multi.ts
+++ assets/multi.ts
@@ -3,11 +3,17 @@ import { createAsset } from "./createAsset";
 const misc = [{
   name: "Done",
   parts: [{
+    mask: "", color: "",
+    body: "M2 2h15v15h-15z",
+  },{
     mask: "", color: "yellow",
-    body: "M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z",
+    body: "M5 5h15v15h-15z",
   },{
     mask: "", color: "blue",
-    body: "M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"
+    body: "M7 7h15v15h-15z",
+  },{
+    mask: "", color: "red",
+    body: "M9 9h15v15h-15z",
   }]
 }];
 
