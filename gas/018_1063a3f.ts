40301b2b6c964370f9f5ee392e78edee600fbab9
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-28 20:08:44 +0900

    gas: after mintWithAsset

---
+++ gas/018_1063a3f.ts
@@ -0,0 +1,23 @@
+
+> nounslove@1.0.0 gas
+> hardhat run ./scripts/gas.ts | sed '1d'
+
+const gas = {
+  action: [
+    { Unit: 886988, ETH: 0.04435, USD: 44.35 },
+    { Unit: 829733, ETH: 0.04149, USD: 41.49 },
+    { Unit: 674053, ETH: 0.0337, USD: 33.7 },
+    { Unit: 622352, ETH: 0.03112, USD: 31.12 },
+    { Unit: 669227, ETH: 0.03346, USD: 33.46 },
+    { Unit: 647640, ETH: 0.03238, USD: 32.38 }
+  ],
+  social: [
+    { Unit: 944387, ETH: 0.04722, USD: 47.22 },
+    { Unit: 987326, ETH: 0.04937, USD: 49.37 },
+    { Unit: 1055417, ETH: 0.05277, USD: 52.77 },
+    { Unit: 761206, ETH: 0.03806, USD: 38.06 },
+    { Unit: 898636, ETH: 0.04493, USD: 44.93 },
+    { Unit: 805779, ETH: 0.04029, USD: 40.29 }
+  ],
+  emoji: [ { Unit: 4254411, ETH: 0.21272, USD: 212.72 } ]
+} ;
