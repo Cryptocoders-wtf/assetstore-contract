05a13e731cdd4c70e93fab2d61a101d2c51ce804
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-30 03:47:01 +0900

    gas: fixed size

---
+++ gas/021_ef7c706.ts
@@ -0,0 +1,37 @@
+
+> nounslove@1.0.0 gas
+> hardhat run ./scripts/gas.ts | sed '1d'
+
+assetStore 0x5FbDB2315678afecb367f032d93F642f64180aa3
+materialToken 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+const gas = {
+  action: [
+    { Unit: 886043, ETH: 0.0443, USD: 44.3 },
+    { Unit: 828788, ETH: 0.04144, USD: 41.44 },
+    { Unit: 673107, ETH: 0.03366, USD: 33.66 },
+    { Unit: 621406, ETH: 0.03107, USD: 31.07 },
+    { Unit: 668317, ETH: 0.03342, USD: 33.42 },
+    { Unit: 646718, ETH: 0.03234, USD: 32.34 }
+  ],
+  social: [
+    { Unit: 943501, ETH: 0.04718, USD: 47.18 },
+    { Unit: 986440, ETH: 0.04932, USD: 49.32 },
+    { Unit: 1054507, ETH: 0.05273, USD: 52.73 },
+    { Unit: 760308, ETH: 0.03802, USD: 38.02 },
+    { Unit: 898470, ETH: 0.04492, USD: 44.92 },
+    { Unit: 804833, ETH: 0.04024, USD: 40.24 }
+  ],
+  emoji: [ { Unit: 4419263, ETH: 0.22096, USD: 220.96 } ],
+  silhouettes: [
+    { Unit: 1418148, ETH: 0.07091, USD: 70.91 },
+    { Unit: 878952, ETH: 0.04395, USD: 43.95 },
+    { Unit: 1106252, ETH: 0.05531, USD: 55.31 },
+    { Unit: 1038137, ETH: 0.05191, USD: 51.91 },
+    { Unit: 1015229, ETH: 0.05076, USD: 50.76 },
+    { Unit: 1129269, ETH: 0.05646, USD: 56.46 }
+  ],
+  crypto: [
+    { Unit: 768077, ETH: 0.0384, USD: 38.4 },
+    { Unit: 785488, ETH: 0.03927, USD: 39.27 }
+  ]
+} ;
