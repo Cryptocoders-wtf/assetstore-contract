24526f0c9cc46f4847aac13122d988acafb30b4a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-30 08:51:30 +0900

    deployed to rinkeby

---
+++ deploy/003_fb73cde.ts
@@ -0,0 +1,5 @@
+No need to generate any newer typings.
+network: rinkeby
+assetStore: 0x7072e8bfF38EB37c9b197F7921Fecb84c2ac1121
+materialToken: 0x51CCdac3F0b999bC2d2ab298c3e1Ad5e4BF83842
+gasUsed: 1090324
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,6 +5,7 @@ import { actionAssets, socialAssets } from "../assets/materials";
 async function main() {
   const [owner] = await ethers.getSigners();
   const { assetStore, materialToken } = await deploy();
+  console.log("network:", network.name);
   console.log("assetStore:", assetStore.address);
   console.log("materialToken:", materialToken.address);
 
@@ -12,10 +13,10 @@ async function main() {
   asset.soulbound = owner.address;
   const tx = await materialToken.mintWithAsset(asset, 0);
   const result = await tx.wait();
-  const summary = result.events.map((event:any) => { return event.event });
+  //const summary = result.events.map((event:any) => { return event.event });
 
-  console.log(summary);
-  console.log(result);
+  //console.log(summary);
+  console.log("gasUsed:", result.gasUsed.toNumber());
 }
 
 main().catch((error) => {
