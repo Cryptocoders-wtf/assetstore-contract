e96f2f061d58748665eeae447577989cf3a35c6c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-07 06:52:01 +0900

    deploy only

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,6 @@
 import { ethers, network } from "hardhat";
 import { deploy } from "../utils/deploy";
-import { socialAssets } from "../assets/materials";
+//import { socialAssets } from "../assets/materials";
 
 async function main() {
   const [owner] = await ethers.getSigners();
@@ -9,14 +9,13 @@ async function main() {
   console.log(`      storeAddress="${assetStore.address}"`);
   console.log(`      tokenAddress="${materialToken.address}"`);
 
+  /*
   const asset = socialAssets[5];
   asset.soulbound = owner.address;
   const tx = await materialToken.mintWithAsset(asset, 0);
   const result = await tx.wait();
-  //const summary = result.events.map((event:any) => { return event.event });
-
-  //console.log(summary);
   console.log("gasUsed:", result.gasUsed.toNumber());
+  */
 }
 
 main().catch((error) => {
