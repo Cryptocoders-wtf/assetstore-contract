e40ae1d958b302ec7d5b2ab8f22ce671dbb3239c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-06 20:33:52 +0900

    xml parser

--- utils/material.ts
+++ utils/material.ts
@@ -1,8 +1,16 @@
 import { readdirSync, readFileSync } from "fs";
+import { XMLParser } from "fast-xml-parser";
 
-const regex = /d="/
+const options = {
+  ignoreAttributes: false,
+  format: true,
+};
+
+const parser = new XMLParser(options);
+
 const path = '../material/src/alert';
 const files = readdirSync(path);
+console.log(files);
 files.map((file) => {
   const path2 = `${path}/${file}/materialicons`;
   let files2 = readdirSync(path2);
@@ -10,6 +18,12 @@ files.map((file) => {
     return parseInt(f0) > parseInt(f1) ? -1 : 1 ;
   });
   let xml = readFileSync(`${path2}/${files2[0]}`, 'utf8');
-  xml = xml.substring(xml.search(regex) + 2);
-  console.log(file, xml);
+  const obj = parser.parse(xml);
+  const paths = obj.svg.path;
+  console.log("***", file, paths);
+  /*
+  paths.map((path2:any) => {
+    console.log(path2);
+  });
+  */
 });
