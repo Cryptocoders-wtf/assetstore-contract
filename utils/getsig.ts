4173d7d0437d9ca0dad1bfd6e9ade90cbcdcf878
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-09 21:32:29 +0900

    hash

--- utils/getsig.ts
+++ utils/getsig.ts
@@ -1,11 +1,13 @@
 import { ethers } from "hardhat";
 import { addresses } from "../cache/addresses_localhost";
 
+const encoder = new TextEncoder();
+
 async function main() {
   // We get the contract to deploy
   const factory = await ethers.getContractFactory("MaterialToken");
   const contract = factory.attach(addresses.tokenAddress);
-  Object.keys(contract.functions).map((k)=>console.log(k));
+  Object.keys(contract.functions).map((k)=>console.log(ethers.utils.keccak256(encoder.encode(k)).substring(0,10),  k));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
