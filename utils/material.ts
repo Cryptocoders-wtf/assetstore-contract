cfae8b073188e83d7ff62d43a9cc5192a8b0d261
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-11 15:27:10 +0900

    Alert icons

--- utils/material.ts
+++ utils/material.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'action') {
+  if (category == 'alert') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
@@ -46,7 +46,7 @@ categories.map(category => {
         process.exit(0);
       }
     });
-    console.log(`export const action50 = ${JSON.stringify(items)} ;`);
+    console.log(`export const assets = ${JSON.stringify(items)} ;`);
   }
 });
   /*
