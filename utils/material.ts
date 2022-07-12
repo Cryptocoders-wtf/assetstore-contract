06d119d78f177785555e42ae524e8dce5d1b9f0d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-12 21:37:27 +0900

    commumication all

--- utils/material.ts
+++ utils/material.ts
@@ -21,7 +21,7 @@ categories.map(category => {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
-      return index < 75;
+      return index < 100;
     }).map((file, index) => {
       if (file == '.DS_Store') {
         return;
