8a3a4173261d759e881a0bed4d5172ef5380cb8b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-21 16:33:39 -0700

    gas for decoder4

--- gas/Decoder4_Kamon_4_13.ts
+++ gas/Decoder4_Kamon_4_13.ts
@@ -1,11 +1,18 @@
 const gas = {
   kamons: [
-    // Shogun (no merge)
+    // Shogun (no merge) index>=4 && index<9
     { Unit: 948943, ETH: 0.01898, USD: 18.98 },
     { Unit: 642803, ETH: 0.01286, USD: 12.86 },
     { Unit: 1612321, ETH: 0.03225, USD: 32.25 },
     { Unit: 811260, ETH: 0.01623, USD: 16.23 },
     { Unit: 1224045, ETH: 0.02448, USD: 24.48 },
+
+    // Shogun (merge)
+    { Unit: 945253, ETH: 0.01891, USD: 18.91 },
+    { Unit: 643144, ETH: 0.01286, USD: 12.86 },
+    { Unit: 1607317, ETH: 0.03215, USD: 32.15 },
+    { Unit: 818716, ETH: 0.01637, USD: 16.37 },
+    { Unit: 1247526, ETH: 0.02495, USD: 24.95 },
 
     // Shogun (1)
     { Unit: 830203, ETH: 0.0166, USD: 16.6 },
