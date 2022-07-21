bb10a8480387ba5c8da3d58fe9622a02ce952ff9
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-21 09:23:45 -0700

    decoder1 kamon 4_9

---
+++ gas/Decoder1_Kamon_4_9.ts
@@ -0,0 +1,10 @@
+No need to generate any newer typings.
+const gas = {
+  kamons: [
+    { Unit: 830203, ETH: 0.0166, USD: 16.6 },
+    { Unit: 564664, ETH: 0.01129, USD: 11.29 },
+    { Unit: 1383466, ETH: 0.02767, USD: 27.67 },
+    { Unit: 713536, ETH: 0.01427, USD: 14.27 },
+    { Unit: 1051818, ETH: 0.02104, USD: 21.04 }
+  ]
+} ;
