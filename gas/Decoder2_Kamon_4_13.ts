76f3531ac1a1401aaef57524162176e61e69c849
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-21 09:19:10 -0700

    decode2 kamon 4-13

---
+++ gas/Decoder2_Kamon_4_13.ts
@@ -0,0 +1,14 @@
+No need to generate any newer typings.
+const gas = {
+  kamons: [
+    { Unit: 807023, ETH: 0.01614, USD: 16.14 },
+    { Unit: 552609, ETH: 0.01105, USD: 11.05 },
+    { Unit: 1340094, ETH: 0.0268, USD: 26.8 },
+    { Unit: 708009, ETH: 0.01416, USD: 14.16 },
+    { Unit: 1042892, ETH: 0.02086, USD: 20.86 },
+    { Unit: 189409, ETH: 0.00379, USD: 3.79 },
+    { Unit: 4048360, ETH: 0.08097, USD: 80.97 },
+    { Unit: 1051019, ETH: 0.02102, USD: 21.02 },
+    { Unit: 1386981, ETH: 0.02774, USD: 27.74 }
+  ]
+} ;
