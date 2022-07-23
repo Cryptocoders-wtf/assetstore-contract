e9bed5478745c79c0375a6f90b374cbf6fad842a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-23 05:38:48 -0700

    records

---
+++ records/store_addresses.ts
@@ -0,0 +1,7 @@
+export const addresses = {
+    storeAddress:"0x847A044aF5225f994C60f43e8cF74d20F756187C",
+    tokenAddresses: {
+        material:"0xd724c63ADaC76Bf7632609dFc808b606c5811b35",
+        kamon:"0x8a7376bb3BaEa9d697E078E3168189D49b9cCEeB"
+    }
+  }
