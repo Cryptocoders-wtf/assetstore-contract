da0dc51e6c4c38c546f7f8a0935c86dbcdd7bddc
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-27 21:09:59 -0700

    removed duplicate

--- utils/emoji.ts
+++ utils/emoji.ts
@@ -22,7 +22,7 @@ categories.map(category => {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
-      return index>=0 && index < 80;
+      return index>=0 && index < 79;
     })
     .filter((file, index) =>{ return file != '.DS_Store'; })
     .map((file, index) => {
