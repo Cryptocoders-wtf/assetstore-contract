041808387e4d7b2a62698373bb2abf852b55eb32
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-28 16:54:15 -0700

    url in getsig

--- utils/getsig.ts
+++ utils/getsig.ts
@@ -5,9 +5,11 @@ const encoder = new TextEncoder();
 
 async function main() {
   // We get the contract to deploy
-  const factory = await ethers.getContractFactory("MaterialToken");
+  const factory = await ethers.getContractFactory("CustomToken");
   const contract = factory.attach(addresses.tokenAddress);
   Object.keys(contract.functions).map((k)=>console.log(ethers.utils.keccak256(encoder.encode(k)).substring(0,10),  k));
+
+  // https://docs.metamask.io/guide/registering-function-names.html
 }
 
 // We recommend this pattern to be able to use async/await everywhere
