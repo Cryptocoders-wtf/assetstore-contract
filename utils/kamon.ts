80bdc940f48a602242e68624e515e435a21a0df7
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-06 19:57:06 -0700

    Igeta kamon

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'ikari') {
+  if (category == 'igeta') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
