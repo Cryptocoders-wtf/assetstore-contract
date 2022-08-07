d249db71a6aee2a6dbb70046a09bee5d50f69426
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-06 20:25:01 -0700

    ishi kamon

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'igeta') {
+  if (category == 'ishi') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
