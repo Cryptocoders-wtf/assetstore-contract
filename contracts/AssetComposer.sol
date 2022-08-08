6736af05ac68e9001749f9abf7b4e494e734600d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-08 09:38:20 -0700

    compiles

--- contracts/AssetComposer.sol
+++ contracts/AssetComposer.sol
@@ -10,10 +10,11 @@ contract AssetComposer is Ownable {
   using Strings for uint256;
 
   IAssetStore public immutable assetStore;
+  uint256 public nextId;
 
   mapping(uint256 => uint256[]) private assets; // compositeId => [assetIds]
-  mapping(uint256 => mapping(uint256 => string)) private transforms;
-  mapping(uint256 => mapping(uint256 => string)) private colors;
+  mapping(uint256 => mapping(uint256 => bytes)) private transforms;
+  mapping(uint256 => mapping(uint256 => bytes)) private fills;
 
   constructor(
     IAssetStore _assetStore
@@ -21,8 +22,35 @@ contract AssetComposer is Ownable {
     assetStore = _assetStore;
   }
 
-  function generateSVGPart(uint256 _compositeId) public view returns(string memory, string memory) {
-    uint256[] memory assetIds = assets[_compositeId];
+  struct AssetInfo {
+    uint256 assetId; // either compositeId or assetId
+    bool isComposition;   
+    string fill; // optional fill color
+    string transform; // optinal transform
+  }
+
+  function register(AssetInfo[] memory _infos) external returns(uint256) {
+    uint256 compositionId = nextId++;
+    //uint256 assetCount = assetStore.getAssetCount();
+    uint256 i;
+    uint256[] storage assetIds = assets[compositionId];
+    for (i=0; i<_infos.length; i++) {
+      AssetInfo memory info = _infos[i];
+      uint256 assetId = info.assetId;
+      if (info.isComposition) {
+        require(assetId < nextId, "register: Invalid compositionId");
+        assetId *= 2; // @notice
+      } else {
+        //require(assetId < assetCount, "register: Invalid assetId");
+        assetId = assetId * 2 + 1; // @notice
+      }
+      assetIds.push(assetId);
+    }
+    return compositionId;
+  }
+
+  function generateSVGPart(uint256 _compositionId) public view returns(string memory, string memory) {
+    uint256[] memory assetIds = assets[_compositionId];
     uint256 i;
     bytes memory defs;
     bytes memory uses;
@@ -31,16 +59,25 @@ contract AssetComposer is Ownable {
     for (i=0; i < assetIds.length; i++) {
       uint256 assetId = assetIds[i];
       if (assetId % 2 == 0) {
-        (svgPart, tagId) = generateSVGPart(assetId);
+        (svgPart, tagId) = generateSVGPart(assetId/2);
       } else {
         IAssetStore.AssetAttributes memory attr = assetStore.getAttributes(assetId/2);
         tagId = attr.tag;
         svgPart = assetStore.generateSVGPart(assetId/2, tagId);
       }
       defs = abi.encodePacked(defs, svgPart);
-      uses = abi.encodePacked(uses, ' <use href="#', tagId, '" />\n');
+      uses = abi.encodePacked(uses, ' <use href="#', tagId, '"');
+      bytes memory transform = transforms[_compositionId][assetId];
+      if (transform.length > 0) {
+        uses = abi.encodePacked(uses, ' transform="', transform, '"');
+      }
+      bytes memory fill = fills[_compositionId][assetId];
+      if (fill.length > 0) {
+        uses = abi.encodePacked(uses, ' fill="', fill, '"');
+      }
+      uses = abi.encodePacked(uses, ' />\n');
     }
-    tagId = string(abi.encodePacked('compo', _compositeId.toString()));
+    tagId = string(abi.encodePacked('comp', _compositionId.toString()));
     svgPart = string(abi.encodePacked(
       defs,
       '<g id="', tagId, '" >\n',
