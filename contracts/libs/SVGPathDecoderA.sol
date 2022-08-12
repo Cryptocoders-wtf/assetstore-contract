0f84448f5d3e9eeececefdf68f8fba7bd24f95d9
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-11 21:20:32 -0700

    removed unused reference to Strings

--- contracts/libs/SVGPathDecoderA.sol
+++ contracts/libs/SVGPathDecoderA.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "../interfaces/IPathDecoder.sol";
 
 contract SVGPathDecoderA is IPathDecoder {
