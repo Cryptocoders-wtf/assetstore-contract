83699c1bce528b61768210dbf9dfce8f5d58436e
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-12 19:53:59 -0700

    inazuma

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'itomaki') {
+  if (category == 'inazuma') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
