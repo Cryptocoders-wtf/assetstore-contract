8ebc8aea21b491154063cf7534a01fbeed0d4754
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-13 22:02:15 -0700

    simplified deploy script

--- scripts/deploy_draw.ts
+++ scripts/deploy_draw.ts
@@ -7,20 +7,17 @@ async function main() {
   const storeFactory = await ethers.getContractFactory("AssetStore");
   const assetStore = storeFactory.attach(storeAddress);
 
-  const assestStoreProviderFactory = await ethers.getContractFactory("AssetStoreProvider");
-  const assetStoreProvider = await assestStoreProviderFactory.deploy(storeAddress);
-  await assetStoreProvider.deployed();
-  console.log(`      assetStoreProvider="${assetStoreProvider.address}"`);
+  const factory = await ethers.getContractFactory("DrawYourOwn");
+  const tokenContract = await factory.deploy(storeAddress, storeAddress, developer, proxy);
+  await tokenContract.deployed();
+  console.log(`      tokenAddress="${tokenContract.address}"`);
 
+  const tx9 = await assetStore.setWhitelistStatus(tokenContract.address, true);
+  await tx9.wait();
+
+  const composerAddress = await tokenContract.assetComposer();
   const composerFactory = await ethers.getContractFactory("AssetComposer");
-  const composer = await composerFactory.deploy(storeAddress);
-  await composer.deployed();
-  console.log(`      composer="${composer.address}"`);
-
-  const tx1 = await composer.registerProvider({name:"asset", provider: assetStoreProvider.address});
-  await tx1.wait();
-  const tx2 = await composer.registerProvider({name:"comp", provider: composer.address});
-  await tx2.wait();
+  const composer = composerFactory.attach(composerAddress);
 
   const count = await composer.providerCount();
   console.log(` providerCount=`, count.toNumber());
@@ -31,13 +28,6 @@ async function main() {
   //const svgPart = await assetStoreProvider.generateSVGPart(0);
   //console.log(` svgPart=`, svgPart);
   
-  const factory = await ethers.getContractFactory("DrawYourOwn");
-  const tokenContract = await factory.deploy(storeAddress, storeAddress, developer, proxy, composer.address);
-  await tokenContract.deployed();
-  console.log(`      tokenAddress="${tokenContract.address}"`);
-
-  const tx9 = await assetStore.setWhitelistStatus(tokenContract.address, true);
-  await tx9.wait();
 
   const addresses = `export const token_addresses = {\n`
   + `  customTokenAddress:"${tokenContract.address}",\n`
