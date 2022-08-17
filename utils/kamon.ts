084ace25ac100586cf6f84d353c86727ac2c0f06
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-17 14:23:20 -0700

    kai kamons

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'ougi') {
+  if (category == 'kai') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
