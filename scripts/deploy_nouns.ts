e13fd277367a202823ce653cf810e320c455be2a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-22 10:56:22 -0700

    registerProvider

--- scripts/deploy_nouns.ts
+++ scripts/deploy_nouns.ts
@@ -1,8 +1,17 @@
 import { ethers, network } from "hardhat";
-import { developer, proxy } from "../utils/deploy";
-import { storeAddress } from "../utils/storeAddress";
-import { writeFile } from "fs";
 import { ChainId, getContractAddressesForChainOrThrow, getContractsForChainOrThrow } from '@nouns/sdk';
+import { token_addresses as local_addresses } from '../cache/addresses_draw_localhost';
+import { token_addresses as rinkeby_addresses } from '../cache/addresses_draw_rinkeby';
+
+const getComposerAddress = () => {
+  if (network.name == "localhost") {
+    return local_addresses.composerAddress;
+  } else if (network.name == "rinkeby") {
+    return rinkeby_addresses.composerAddress;
+  }
+  return "error";
+};
+const composerAddress = getComposerAddress();
 
 const chainId = (network.name == "rinkeby") ? ChainId.Rinkeby : ChainId.Mainnet;
 console.log("network", network.name, chainId);
@@ -10,10 +19,17 @@ const { nounsDescriptor } = getContractAddressesForChainOrThrow(chainId);
 console.log("nounsDescriptor", nounsDescriptor);
 
 async function main() {
-  const factory = await ethers.getContractFactory("NounsAssetProvider");
-  const nounsProvider = await factory.deploy(nounsDescriptor);
+  const nounsFactory = await ethers.getContractFactory("NounsAssetProvider");
+  const nounsProvider = await nounsFactory.deploy(nounsDescriptor);
   await nounsProvider.deployed();
   console.log(`      nounsProvider="${nounsProvider.address}"`);
+
+  const composerFactory = await ethers.getContractFactory("AssetComposer");
+  const composer = composerFactory.attach(composerAddress);
+
+  const tx = await composer.functions.registerProvider(nounsProvider.address);
+  const result = await tx.wait();
+  console.log("events", result.events);
 }
 
 main().catch((error) => {
