df9c4a52267aa80599b19344c8a147a6f39f97b7
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-22 08:13:42 -0700

    kamon/kane

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'karigane') {
+  if (category == 'kagi') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
