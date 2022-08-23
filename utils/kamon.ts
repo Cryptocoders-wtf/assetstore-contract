01202f0bc96613bc98203d459a6aa71f028e7a82
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-23 15:28:48 -0700

    karabana

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'kagi') {
+  if (category == 'karabana') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
