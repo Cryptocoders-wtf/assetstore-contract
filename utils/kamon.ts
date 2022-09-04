f54b460055c78ae32b98bd4eec93cfd1b6008e8f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-04 05:27:43 -0700

    kikkou

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'kan') {
+  if (category == 'kikkou') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
