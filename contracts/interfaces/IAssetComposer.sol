8903848b0937b153d77af6eedbdf733a56b631e4
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-09 15:45:22 -0700

    comments

--- contracts/interfaces/IAssetComposer.sol
+++ contracts/interfaces/IAssetComposer.sol
@@ -6,11 +6,17 @@ import { IAssetProvider } from './IAssetProvider.sol';
 
 /**
  * IAssetProviderRegistry is the interface implemented by AssetCompoer, which allows developers
- * of various asset providers to register those providers to AssetComposer. 
+ * of asset providers to register those providers to AssetComposer.
  */
 interface IAssetProviderRegistry {
   event ProviderRegistered(address from, uint256 _providerId);
   event ProviderUpdated(address from, uint256 _providerId);
+
+  /**
+   * This function registers the specified provider to the provider registry.
+   * It is possible to update it using the same key, but the owner of the new provider
+   * must match the previous owner.
+   */
   function registerProvider(IAssetProvider _provider) external returns(uint256);
   function providerCount() external view returns(uint256);
   function getProvider(uint256 _providerId) external view returns(IAssetProvider.ProviderInfo memory);
@@ -26,14 +32,15 @@ interface IAssetComposer {
    * AssetLayer represents a layer of a composition. 
    */
   struct AssetLayer {
-    uint256 assetId; // assetId
-    string provider; // provider key   
-    string fill; // optional fill color
+    uint256 assetId;  // assetId
+    string provider;  // provider key
+    string fill;      // optional fill color
     string transform; // optinal transform
-    uint256 stroke; // optional stroke (width)
+    uint256 stroke;   // optional stroke (width)
   }
 
   event CompositionRegistered(address from, uint256 compositionId);
+
   function registerComposition(AssetLayer[] memory _layers) external returns(uint256);
   function generateSVGPart(uint256 _compositionId) external view returns(string memory, string memory);
 }
