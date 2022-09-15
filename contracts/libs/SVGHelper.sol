87f884236b556cb530afa8cfa1f6e720e69e04c7
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-15 10:22:51 -0700

    SVGHelper

---
+++ contracts/libs/SVGHelper.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ * Helper methods for SVG generative.
+ *
+ * Created by Satoshi Nakajima (@snakajima)
+ */
+
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract SVGHelper {
+  using Strings for uint32;
+
+  struct Point {
+    int32 x;
+    int32 y;
+    bool c;   // true:line, false:bezier
+    int32 r; // ratio (0 to 1024)
+  }
+
+  /**
+   * Generate a SVG path from series of Points
+   */
+  function PathFromPoints(Point[] memory points) public pure returns(bytes memory) {
+    bytes memory ret;
+    uint256 length = points.length;
+    for(uint256 i = 0; i < length; i++) {
+      Point memory point = points[i];
+      Point memory prev = points[(i + length - 1) % length];
+      int32 sx = (point.x + prev.x) / 2;
+      int32 sy = (point.y + prev.y) / 2;
+      if (i == 0) {
+        ret = abi.encodePacked("M", uint32(sx).toString(), ",", uint32(sy).toString());
+      }
+      if (point.c) {
+        ret = abi.encodePacked(ret, "L", uint32(point.x).toString(), ",", uint32(point.y).toString());
+      } else {
+        Point memory next = points[(i + 1) % length];
+        int32 ex = (point.x + next.x) / 2;
+        int32 ey = (point.y + next.y) / 2;
+        ret = abi.encodePacked(ret, "C",
+          uint32(sx + point.r * (point.x - sx) / 1024).toString(), ",",
+          uint32(sy + point.r * (point.y - sy) / 1024).toString(), ",",
+          uint32(ex + point.r * (point.x - ex) / 1024).toString(), ",",
+          uint32(ey + point.r * (point.y - ey) / 1024).toString(), ",",
+          uint32(ex).toString(), ",", uint32(ey).toString());
+      }
+    }
+    return ret;
+  }  
+}
