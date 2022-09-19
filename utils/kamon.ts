2da8360892c46018dbebab7c75bd454707d1cd03
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-19 08:08:02 -0700

    sakura

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'koma') {
+  if (category == 'sakura') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
