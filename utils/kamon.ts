80e55e39ada564d655477f5f568d5ed7d76f5c33
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-08 15:37:56 -0700

    rotate

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'jigami') {
+  if (category == 'janome') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
