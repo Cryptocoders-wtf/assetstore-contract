64483900e4371ae3ebc186676427f075889acb49
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-22 09:40:24 -0700

    10 more emoji

--- utils/kamon.ts
+++ utils/kamon.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'janome') {
+  if (category == 'suhama') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
