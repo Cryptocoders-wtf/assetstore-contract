f5d3e328e692efb78b3b912f5e4f78e0f5fe29cd
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-25 15:45:26 -0700

    reddit

--- utils/material.ts
+++ utils/material.ts
@@ -17,7 +17,7 @@ categories.map(category => {
   }
   let files = readdirSync(`${root}/${category}`);
   //console.log(category, files.length);
-  if (category == 'logos') {
+  if (category == 'crypto') {
     let files = readdirSync(`${root}/${category}`);
     //console.log(files);
     const items = files.filter((file, index) => {
